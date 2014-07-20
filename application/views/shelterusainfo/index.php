d25933c30e8cbed9a2d509e86270d3843e2117da
Gonzalo Díaz <gdiaz@starmount.com>
2014-07-20 15:08:46 -0300

    info de refugios

--- application/views/shelterusainfo/index.php
+++ application/views/shelterusainfo/index.php
@@ -1,53 +1,82 @@
 <div class="right">
+  <table border=1>
+    <tr>
+      <td colspan="2" style="padding-top:10px;padding-bottom:10px;">
+        <?php 
+          list($width, $height, $type, $attr) = getimagesize( $GLOBALS['raizCms'] .  "/resources/images/shelterlogos/" . $info->getLogoUrl());
 
-  <br/>
-  <br/>
-  <img class="infoDetail" src='<?php echo  $GLOBALS['dirAplicacion']  . "/resources/images/shelterlogos/" . $info->getLogoUrl() ?>'>
-  <br/>
-  <br/>
-  <div class="pictureTitleInfo"><?php echo $info->getName(); ?></div>
-  <br/>
-  <br/>
-    <?php echo $info->getDescription(); ?> 
-  <br/>
-  <br/>
-  <h1>Contact information</h1>
-  <br/>
-  <?php
-    $phone = $info->getPhone();
-    if (!empty($phone)){
-      echo "Phone:" . $phone;
-      echo "<br/>";
-      echo "<br/>";
-    }
-  ?>
-  <?php
-    $email = $info->getEmail();
-    if (!empty($email)){
-      echo "Email:<a href='mailto:" . $email . "'>" . $email . "</a>";
-      echo "<br/>";
-      echo "<br/>";
-    }
-  ?>
-  <?php
-    $url = $info->getUrl();
-    if (!empty($url)){
-      echo "Website:<a href='" . $url . "'>" . $url . "</a>";
-      echo "<br/>";
-      echo "<br/>";
-    }
-  ?>
-  <h2>Address</h2>
-  <br/>
-  <?php echo $info->getStreetAddress(); ?> 
-  <br/>
-  <?php echo $info->getCityName(); ?>,&nbsp;&nbsp;<?php echo $info->getStateName(); ?>&nbsp;&nbsp;<?php echo $info->getZip(); ?> 
-  <br/>
-  <br/>  
-  <span class="navegacionPaginas">
-	    <?php 
-	      echo "  <a href='" . URL . "/sheltersusa'> << Back to List </a> \n";
-	    ?>
-  </span>
+          if ($width>(2 * $height)){
+            $estiloImagen="detailShelterAncha";
+          }else{
+            $estiloImagen="detailShelter";
+          }
+          ?>
+          <img class="<?php echo $estiloImagen; ?>"  src='<?php echo  $GLOBALS['dirAplicacion']  . "/resources/images/shelterlogos/" . $info->getLogoUrl() ?>'>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" >
+        <div class="pictureTitleInfo"><?php echo $info->getName(); ?></div>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" style="padding-top:10px;padding-bottom:10px;">
+        <div class="infoText"> <?php echo $info->getDescription(); ?></div> 
+      </td>
+    </tr>
+    <tr>
+      <td style="width:400px;height:600px">
+        <div id="map-canvas" style="height:100%"></div>   
+      </td>
+      <td style="width:400px;height:600px" valign="top">
+        <div class="pictureTitleInfo">Contact information</div>
+			  <br/>
+			  <?php
+			    $phone = $info->getPhone();
+			    if (!empty($phone)){
+			      echo "Phone:" . $phone;
+			      echo "<br/>";
+			      echo "<br/>";
+			    }
+			  ?>
+			  <?php
+			    $email = $info->getEmail();
+			    if (!empty($email)){
+			      echo "Email:<a href='mailto:" . $email . "'>" . $email . "</a>";
+			      echo "<br/>";
+			      echo "<br/>";
+			    }
+			  ?>
+			  <?php
+			    $url = $info->getUrl();
+			    if (!empty($url)){
+			      echo "Website:<a href='" . $url . "'>" . $url . "</a>";
+			      echo "<br/>";
+			      echo "<br/>";
+			    }
+			  ?>
+			  <h2>Address</h2>
+			  <br/>
+			  <?php echo $info->getStreetAddress(); ?> 
+			  <br/>
+			  <?php echo $info->getCityName(); ?>,&nbsp;&nbsp;<?php echo $info->getStateName(); ?>&nbsp;&nbsp;<?php echo $info->getZip(); ?> 
+			  <br/>
+			  <br/>  
+         
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <span class="navegacionPaginas">
+	      <?php 
+	        echo "  <a href='" . URL . "/sheltersusa'> << Back to List </a> \n";
+	      ?>
+        </span> 
+      </td>
+    </tr>
+  </table>
+  
+  
+  
   
 </div>
--- config.php
+++ config.php
@@ -5,6 +5,7 @@
 //  global $usuario; $usuario ='petzynga_gonzalo';
 //    global $clave; $clave ='manuela';
 //    global $baseDeDatos; $baseDeDatos ='petzynga_cmsqa';
+//    $GLOBALS['raizCms'] ='/home1/petzynga/public_html/qacms';
 //    $GLOBALS['dirWeb'] ='/qaweb';
 //    $GLOBALS['dirAplicacion'] ='/qacms';
 //    $GLOBALS['host'] ='petzynga.com';
@@ -15,6 +16,7 @@
   global $usuario; $usuario ='almarlam_gonzalo';
   global $clave; $clave ='manuela';
   global $baseDeDatos; $baseDeDatos ='petzynga';
+  $GLOBALS['raizCms'] ='C:/xampp/htdocs/petzyngacms';
   $GLOBALS['dirWeb'] ='/petzyngaweb';
   $GLOBALS['dirAplicacion'] ='/petzyngacms';
   $GLOBALS['host'] ='localhost';

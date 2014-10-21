f73ea96d7fb7c96b515072e81ee12ceef324ccde
gdiazpromeba <dongonzalito@gmail.com>
2014-10-21 00:51:42 -0200

    página de países de breeders

--- application/views/breeders/countries.php
+++ application/views/breeders/countries.php
@@ -1,9 +1,13 @@
  <?php require_once 'utils/Resources.php';?>
- <div class="descriptiveParagraph2A"><?php echo Resources::getTeasers('adopt_responsibly'); ?></div>
- <br/>
  <table border="1" style="padding:5px;">
    <tr>
-     <td style="vertical-align">
+     <td colspan="4" style="text-align: left">
+       <span style="font-family: Oswald; font-color:olive; font-size:16px;margin:5px;">Responsible Breeders</span>
+       <div style="text-align:justify;font-size:small;padding:5px" ><?php echo Resources::getText('responsible_breeders'); ?></div>
+     </td>
+   </tr>
+   <tr>
+     <td>
        <a class="noUnderline" href="<?php echo URL; ?>breeders/listing/usa/initial">
          <img class="menuCountries" src="<?php echo $GLOBALS['dirAplicacion'] .  "/resources/images/usa_map_menu_outline.jpeg"; ?>" />
          <div class="itemMenuCountries"><?php echo Resources::getText('breeders_in_the_usa'); ?></div>
@@ -17,13 +21,15 @@
          <div class="itemMenuCountries"><?php echo Resources::getText('number_breeders_available', $shelterCount["canada"]); ?></div>
        </a>
      </td>
-     <td colspan="2" rowspan="2">
-       <div style="text-align:justify;font-size:small" ><?php echo Resources::getText('puppy_mill_warning_1'); ?></div>
+     <td colspan="2" rowspan="2" style="text-align: left">
+       <span style="font-family: Oswald; font-color:olive; font-size:16px;margin:5px;">Beware of "Puppy Mills"</span>
+       <div style="text-align:justify;font-size:small;padding:5px" ><?php echo Resources::getText('puppy_mill_warning_1'); ?></div>
      </td>
    </tr>
    <tr>
-     <td colspan="2">
-       <div><?php echo Resources::getText('puppy_mill_warning_2'); ?></div>
+     <td colspan="2" style="text-align: left">
+       <span style="font-family: Oswald; font-color:olive; font-size:16px;margin:5px">See through the sales pitch</span>
+       <div style="text-align:justify;font-size:small;padding:5px"><?php echo Resources::getText('puppy_mill_warning_2'); ?></div>
      </td>
     </tr>
  </table>
